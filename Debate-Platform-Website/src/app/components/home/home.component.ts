a6a8f0ee13cf977c82321b188f1e86593a88633c
Watsa J <wjwatsa@gmail.com>
2021-08-07 10:58:33 +0700

    home page edited (not final)

--- Debate-Platform-Website/src/app/components/home/home.component.ts
+++ Debate-Platform-Website/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,18 @@ import { FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   roomId = new FormControl()
 
   ngOnInit(): void {
   }
+
+  toLogIn(){
+    this.router.navigate(['login']);
+  }
+
 
 }
