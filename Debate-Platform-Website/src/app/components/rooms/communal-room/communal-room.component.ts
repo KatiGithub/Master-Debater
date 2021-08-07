f7163561f59a9b371fed8ab67a20423f41da00b6
kati <kati.pairojtanachai@gmail.com>
2021-08-07 17:12:44 +0700

    Fixed communal room import bug

--- Debate-Platform-Website/src/app/components/rooms/communal-room/communal-room.component.ts
+++ Debate-Platform-Website/src/app/components/rooms/communal-room/communal-room.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore'
-import { ActivatedRoute, Params } from '@angular/router'
-import { FirestoreService } from '../../../services/Firestore/firestore.service'
+import { ActivatedRoute, Params } from '@angular/router';
+import { FirestoreService } from '../../../services/Firestore/firestore.service';
 
 interface Format {
   value: string;
@@ -20,53 +19,53 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079},
-  {position: 2, name: 'Helium', weight: 4.0026},
-  {position: 3, name: 'Lithium', weight: 6.941},
-  {position: 4, name: 'Beryllium', weight: 9.0122},
-  {position: 5, name: 'Boron', weight: 10.811}
+  { position: 1, name: 'Hydrogen', weight: 1.0079 },
+  { position: 2, name: 'Helium', weight: 4.0026 },
+  { position: 3, name: 'Lithium', weight: 6.941 },
+  { position: 4, name: 'Beryllium', weight: 9.0122 },
+  { position: 5, name: 'Boron', weight: 10.811 },
 ];
 
 @Component({
   selector: 'app-communal-room',
   templateUrl: './communal-room.component.html',
-  styleUrls: ['./communal-room.component.css']
+  styleUrls: ['./communal-room.component.css'],
 })
-
 export class CommunalRoomComponent implements OnInit {
-  selectedValue!: string;
-  selectedTime!: string;
-    
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  courtId: string = ''
   
-  authorized: Boolean = false
-
-  constructor(private route: ActivatedRoute,
-              private courtId: string,
-              private firestore: FirestoreService) {
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: FirestoreService
+  ) {
     this.route.params.subscribe((params: Params) => {
       this.courtId = params.court_id;
-      firestore.checkIfHost(courtId).then((value) => {
-        this.authorized = value
-      })
-    })
-  }
-  
-  ngOnInit(): void {
+      firestore.checkIfHost(this.courtId).then((value) => {
+        this.authorized = value;
+      });
+    });
   }
 
+  selectedValue!: string;
+  selectedTime!: string;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  authorized: Boolean = false;
+
+  ngOnInit(): void {}
+
   formats: Format[] = [
-    {value: '1', viewValue: 'Asian Parliament'},
-    {value: '2', viewValue: 'British'},
-    {value: '3', viewValue: ''}
+    { value: '1', viewValue: 'Asian Parliament' },
+    { value: '2', viewValue: 'British' },
+    { value: '3', viewValue: '' },
   ];
 
   times: Time[] = [
-    {value: '1', viewValue: '15 minutes'},
-    {value: '2', viewValue: '30 minutes'},
-    {value: '3', viewValue: '45 minutes'},
-    {value: '4', viewValue: '60 minutes'}
+    { value: '1', viewValue: '15 minutes' },
+    { value: '2', viewValue: '30 minutes' },
+    { value: '3', viewValue: '45 minutes' },
+    { value: '4', viewValue: '60 minutes' },
   ];
 }
-
